8045b6ec26160b3496e1812ede37c4526a763657
Volodymyr Lohvinov <vladimirpravda2@gmail.com>
2023-10-05 14:46:00 +0200

    third commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,111 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin')
+
+
 
 module.exports = {
-  entry: "./src/index.jsx'",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name]-[fullhash].js",
-    clean: true,
-    publicPath: "./",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name]-[fullhash].css",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "static", to: "static" }],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+    mode: 'none',
+    entry: './src/index.jsx',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+      publicPath: './'
     },
-    port: 8064,
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {from: "static", to: "static",}
+        ]
+      }
+
+      )
+     
+    ],
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: "babel-loader",
-            options: {
-              presets: [
-                '@babel/preset-env',
-                '@babel/preset-react',
-              ],
-      },},
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              url: false,
-            },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sassOptions: {
-                includePaths: ["node_modules"],
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  '@babel/preset-env',
+                  '@babel/preset-react',
+                ],
+                
               },
             },
           },
+          {
+            test: /\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              
+              {
+                loader: "css-loader",
+                options: {
+                  esModule: true,
+                  modules: {
+                    namedExport: true,
+                    localIdentName: "[name]__[local]",
+                  },
+                  
+                  },
+                },{
+                  loader: 'sass-loader',
+                  options: {
+                  sassOptions: {
+                    includePaths: ['node_modules'],
+                  }
+                }
+              },
+            ],
+          },
+          {
+            test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
         ],
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "images/",
-            },
+            loader: 'file-loader',
           },
         ],
       },
     ],
   },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
-  },
-};
+    
+      optimization: {
+        minimizer: [
+          new CssMinimizerPlugin(),
+          new TerserPlugin(),
+        ],
+      },
+      devServer: {
+        static: {
+          directory: path.join(__dirname, 'dist'),
+        },
+        port: 8064,
+        historyApiFallback: true,
+        watchFiles: 'src/**/*',
+        devMiddleware: {
+          writeToDisk: true
+        },
+      },
+      };
